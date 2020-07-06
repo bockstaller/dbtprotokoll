10aa7bda73ed39413b0cf1fd44004288f5ccd687
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2020-07-07 01:25:28 +0200

    Unifies variable and parameter naming

--- R/absatzliste.R
+++ R/absatzliste.R
@@ -5,24 +5,21 @@ library(tidyverse)
 #!Not saved information: paragraphs which aren't part of speech are ignored
 #!Bug: moderation from the president/vicepresidents is included and attributet incorrectly. We have to check if they are the only ones who talk without getting a speech element as introduction
 paragraph_list <- function(protokoll){
+paragraph_list <- function(protocol){
 
   #create data frame of fitting shape to collect comments
   paragraph_df <- data.frame(id = integer(),
                          speech = character(),
                          speech_type = character(),
                          speaker_id = integer(),
-                         moderator_id = integer(),
+                         moderator = character(),
                          class = character(),
                          content = character(),
                          j_1_gate = logical()
                   )
 
-  name_df <- data.frame(id = integer(),
-                        name = character()
-             )
-
   # we are only interestet in elements contained in speeches
-  speechlist <- xml_find_all(protokoll, ".//rede/p | .//rede/name")
+  speech_list <- xml_find_all(protocol, ".//rede/p | .//rede/name")
 
   # we assume that paragraphs in a speech section can be grouped into "speech" and "moderation"
   # a speech section always starts with a speaker
@@ -40,29 +37,28 @@ paragraph_list <- function(protokoll){
 
 
 
-  for (i in 1L:length(speechlist)){
+  for (i in 1L:length(speech_list)){
     # we handle a moderation section by appending the name of the moderator to a list
     # which gets deduplicated later and setting some modes
     # we do not reset the current speaker, this way we have a chance to know who gets moderated
     # we do not append to the paragraph list
 
-    if (xml_name(speechlist[i]) == "name"){
-      name_df <- name_df %>% add_row(id=i, name=xml_text(speechlist[i]))
-      current_moderator <- i
+    if (xml_name(speech_list[i]) == "name"){
+      current_moderator <- xml_text(speech_list[i])
       current_speech_type <- 3
       j_1_gate <- FALSE
       next()
     }
 
-    if (xml_name(speechlist[i]) == "p"){
-      class <- xml_attr(speechlist[i], "klasse")
+    if (xml_name(speech_list[i]) == "p"){
+      class <- xml_attr(speech_list[i], "klasse")
 
       # in case of a new speaker
       # we reset the moderator, save the speaker id and change some modes
       # we do not append to the paragraph list
       if (class == "redner"){
         current_moderator <- NA
-        current_speaker <- as.integer(xml_attr(xml_child(speechlist[i], "redner"), "id"))
+        current_speaker <- as.integer(xml_attr(xml_child(speech_list[i], "redner"), "id"))
         current_speech_type <- 2
         j_1_gate <- FALSE
 
@@ -78,33 +74,24 @@ paragraph_list <- function(protokoll){
 
       paragraph_df <- paragraph_df %>% add_row(id = i,
                                        speaker_id = current_speaker,
-                                       moderator_id = current_moderator,
+                                       moderator = current_moderator,
                                        speech_type = speech_types[current_speech_type],
-                                       speech = xml_attr(xml_parent(speechlist[i]), "id"),
+                                       speech = xml_attr(xml_parent(speech_list[i]), "id"),
                                        class = class,
-                                       content = xml_text(speechlist[i]),
+                                       content = xml_text(speech_list[i]),
                                        j_1_gate = j_1_gate)
 
       next()
     }
-
 
   }
 
   #transform data frame to tibble for easier analysis later
   paragraph_tb <- as_tibble(paragraph_df)
 
-  name_tb <- as_tibble(name_tb)
-
   return(paragraph_tb)
 
 }
-
-
-
-
-
-
 
 #testing
 #x <- read_xml("./protokolle/19007-data.xml")
