1f1718eb8b282940af78ee94fc15f6d2403f6458
Grausbert <r.paus@web.de>
2020-07-10 11:54:56 +0200

    removed @export-tag and changed code to match `::`-syntax

--- R/rednerliste.R
+++ R/rednerliste.R
@@ -1,36 +1,31 @@
-library(xml2)
-library(tidyverse)
-
 #usage:
 #speakers(read_xml("protokolle/19003-data.xml"))
 
 #returns a tibble of all the names of the speakers
-#'@export
 speakers <- function(protokoll){
-  speakerlist <- xml_find_all(protokoll, ".//rednerliste")
-  speaker <- xml_find_all(speakerlist, ".//redner")
+  speakerlist <- xml2::xml_find_all(protokoll, ".//rednerliste")
+  speaker <- xml2::xml_find_all(speakerlist, ".//redner")
   speakertbs <- sapply(speaker, name_to_tibble)
 
   #combine single tibbles to one big tibble
   speakertb <- speakertbs[1]
   i <- 2
   while(i <= length(speakertbs)){
-    speakertb <- bind_rows(speakertb, speakertbs[[i]])
+    speakertb <- dplyr::bind_rows(speakertb, speakertbs[[i]])
     i <- i + 1
   }
 
   #bring tibble in neat order, but be careful because of possibility of missing "namenszusatz"-column
   if("namenszusatz" %in% colnames(speakertb)){
-    return(select(speakertb, id, titel, vorname, namenszusatz, nachname, everything()))
+    return(dplyr::select(speakertb, id, titel, vorname, namenszusatz, nachname, everything()))
   }
   else{
-    return(select(speakertb, id, titel, vorname, nachname, everything()))
+    return(dplyr::select(speakertb, id, titel, vorname, nachname, everything()))
   }
 }
 
 #return a tibble with the whole name and id for a given name of a speaker
 name_to_tibble <- function(speaker){
-  enframe(unlist(as_list(xml_find_all(speaker, ".//name")))) %>%
-    pivot_wider(names_from = name, values_from = value) %>%
-    mutate(id = xml_attr(speaker, "id"))
+  dplyr::mutate(tidyr::pivot_wider(tibble::enframe(unlist(xml2::as_list(xml2::xml_find_all(speaker, ".//name")))),
+                                   names_from = name, values_from = value), id = xml2::xml_attr(speaker, "id"))
 }
