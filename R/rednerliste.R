8bd69e3b924f175f53da202f184f6802ceaa9c73
Grausbert <r.paus@web.de>
2020-07-19 17:25:26 +0200

    added spliting roles and tidying them, also improved tidying speakers

--- R/rednerliste.R
+++ R/rednerliste.R
@@ -20,10 +20,10 @@ speakers <- function(protokoll){
 
   #bring tibble in neat order, but be careful because of possibility of missing "namenszusatz"-column
   if("namenszusatz" %in% colnames(speakertb)){
-    return(dplyr::select(speakertb, id, titel, vorname, namenszusatz, nachname, everything()))
+    return(dplyr::select(speakertb, id, any_of("titel"), vorname, namenszusatz, nachname, everything()))
   }
   else{
-    return(dplyr::select(speakertb, id, titel, vorname, nachname, everything()))
+    return(dplyr::select(speakertb, id, any_of("titel"), vorname, nachname, everything()))
   }
 }
 
@@ -46,28 +46,47 @@ unify_fractions <- function(speaker_tbl){
   return(speaker_tbl)
 }
 
+#gives back tibbles for speakers without roles and for roles with id
+split_roles <- function(speakers){
+  if("rolle.rolle_lang" %in% colnames(speakers)){
+    if(!("rolle.rolle_kurz" %in% colnames(speakers))){
+      speakers <- dplyr::mutate(speakers, "rolle.rolle_kurz" = NA_character_)
+    }
+    roles <- dplyr::select(speakers, id, rolle.rolle_lang, rolle.rolle_kurz)
+    roles <- tidyr::drop_na(roles)
+
+    speakers <- dplyr::mutate(speakers, rolle = !is.na(rolle.rolle_lang), rolle.rolle_lang = NULL, rolle.rolle_kurz = NULL)
+  }
+  else{
+    roles <- tibble::tibble("id" = character(), "rolle.rolle_lang" = character(), "rolle.rolle_kurz" = character())
+    speakers <- dplyr::mutate(speakers, rolle = FALSE)
+  }
+  return(list(speakers, roles))
+}
 
 
 #function to tidy up the list of speakers
 clean_speakers <- function(speakers){
-  #tidy multiple names for one id
-  speakers[speakers$id == "10000", 1] <- as.character(10000+ (1:nrow(speakers[speakers$id == "10000",])))
+  #remove 10000-ids
+  wrongids <- which(speakers$id == "10000")
+  if(!identical(wrongids, integer(0))){
+    speakers <- speakers[-which(speakers$id == "10000"),]
+  }
 
   #merge rows of same person into one
-  speakers <- dplyr::group_by(speakers, id) %>% dplyr::mutate("anzahl" = dplyr::n()) %>% dplyr::arrange(dplyr::desc(anzahl), id)
+  speakers <- dplyr::arrange(dplyr::mutate(dplyr::group_by(speakers, id), "anzahl" = dplyr::n()), dplyr::desc(anzahl), id)
   for(rownum in 1:nrow(speakers)){
     if(speakers$anzahl[rownum] > 1){
       for(i in 1:(speakers$anzahl[rownum] - 1)){
-        speakers[rownum,1:(length(speakers)-1)] <- dplyr::full_join(speakers[rownum,], speakers[rownum + i,], by = "id") %>%
-          dplyr::transmute(id,
-                    "titel" = dplyr::coalesce(.$titel.x, .$titel.y),
-                    vorname = dplyr::coalesce(.$vorname.x, .$vorname.y),
-                    nachname = dplyr::coalesce(.$nachname.x, .$nachname.y),
-                    rolle.rolle_lang = dplyr::coalesce(.$rolle.rolle_lang.x, .$rolle.rolle_lang.y),
-                    rolle.rolle_kurz = dplyr::coalesce(.$rolle.rolle_kurz.x, .$rolle.rolle_kurz.y),
-                    ortszusatz = dplyr::coalesce(.$ortszusatz.x, .$ortszusatz.y),
-                    fraktion = dplyr::coalesce(.$fraktion.x, .$fraktion.y),
-                    namenszusatz = dplyr::coalesce(.$namenszusatz.x, .$namenszusatz.y))
+        speakers[rownum,c(1:6,8)] <- dplyr::transmute(dplyr::full_join(speakers[rownum,], speakers[rownum + i,], by = "id"),
+                    id,
+                    titel = dplyr::coalesce(titel.x, titel.y),
+                    vorname = dplyr::coalesce(vorname.x, vorname.y),
+                    nachname = dplyr::coalesce(nachname.x, nachname.y),
+                    ortszusatz = dplyr::coalesce(ortszusatz.x, ortszusatz.y),
+                    fraktion = dplyr::coalesce(fraktion.x, fraktion.y),
+                    namenszusatz = dplyr::coalesce(namenszusatz.x, namenszusatz.y))
+        speakers[rownum,]$rolle <- (speakers[rownum,]$rolle | speakers[rownum + i,]$rolle)
       }
       #remove next entries of group
       speakers <- speakers[-(rownum + (1:(speakers$anzahl[rownum]-1))),]
@@ -79,8 +98,17 @@ clean_speakers <- function(speakers){
 
   #ToDo: Tidy up entries of new entries of titel, namenszusatz and rolle
 
-  return(speakers[1:(length(speakers)-1)] %>% ungroup())
+  return(dplyr::ungroup(speakers[1:(length(speakers)-1)]))
 }
-#redner <- full_protocol[[1]]
-#redner <- clean_speakers(redner)
-#redner %>% arrange(desc(anzahl),id) -> redner
+
+clean_roles <- function(roles){
+  #remove 10000-ids
+  rubishids <- which(roles$id == "10000")
+  if(!identical(rubishids, integer(0))){
+    roles <- roles[-rubishids]
+  }
+
+  roles <- dplyr::mutate(roles, "rolle.rolle_lang" = stringr::str_squish(roles$rolle.rolle_lang), "rolle.rolle_kurz" = stringr::str_squish(roles$rolle.rolle_kurz))
+  roles <- dplyr::distinct(roles)
+  return(roles)
+}
